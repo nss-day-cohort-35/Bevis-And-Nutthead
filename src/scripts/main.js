a4c6f2e41c99484488712510422843d0112cedb8
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-09-19 14:09:23 -0500

    Nav Bar and User Fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
+import dbcalls from "./auth/dbcalls.js"
+
 /*
     Import all the tools into main.js that are needed to display
     the initial UI to the user. Either the login form should appear
     or the dashboard should be rendered.
 */
 
-const message = "Time to build an application that gives you all the information you need in a Nutshell"
+//const message = "Time to build an application that gives you all the information you need in a Nutshell"
 
-document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
-
-console.log(message)
+//const container = document.querySelector("#container").innerHTML
+const test = dbcalls.getUserId().then(data=>{
+    console.log(data);
+})
+//console.log(message)
